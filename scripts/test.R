cbf99649ff3612b2ea0d7b18d888186c94cfe6c2
skitter30 <40482877+leahbalter@users.noreply.github.com>
2020-07-22 00:06:23 -0400

    column names, left_joins
    continuing

--- scripts/test.R
+++ scripts/test.R
@@ -10,7 +10,16 @@ cm.header.row <- here('data', 'cm_header_file.csv') %>%
 
 cm.master.list <- here('data', 'cm.txt') %>%
   read_delim(delim = '|',
-             col_names = cm.header.row)
+             col_names = cm.header.row) %>%
+  select(comm.id = CMTE_ID, comm.name = CMTE_NM,
+         comm.treas = TRES_NM, comm.st.1 = CMTE_ST1,
+         comm.st.2 = CMTE_ST2, comm.city = CMTE_CITY,
+         comm.state = CMTE_ST, comm.zip = CMTE_ZIP,
+         comm.desig = CMTE_DSGN, comm.type = CMTE_TP,
+         comm.party = CMTE_PTY_AFFILIATION,
+         comm.org.type = ORG_TP,
+         comm.connected.org = CONNECTED_ORG_NM,
+         cand.id = CAND_ID)
 
 can.header.row <- here('data', 'cn_header_file.csv') %>%
   read_csv(col_names = FALSE) %>%
@@ -38,7 +47,28 @@ data.header.row <- here('data', 'pas2_header_file.csv') %>%
 
 comm.can.data <- here('data', 'comm-can-2020.txt') %>%
   read_delim(delim = '|', guess_max = 250000,
-             col_names = data.header.row)
+             col_names = data.header.row) %>%
+  select(row.id = SUB_ID, comm.id = CMTE_ID, 
+         ammend.ind = AMNDT_IND, report.type = RPT_TP, 
+         prim.gen.ind = TRANSACTION_PGI,
+         trans.type = TRANSACTION_TP, entity.type = ENTITY_TP,
+         name = NAME, city = CITY, state = STATE,
+         zip = ZIP_CODE, employer = EMPLOYER,
+         occupation = OCCUPATION,
+         trans.date = TRANSACTION_DT,
+         trans.amount = TRANSACTION_AMT,
+         other.id = OTHER_ID, can.id = CAND_ID,
+         memo.code = MEMO_CD, memo.text = MEMO_TEXT)
 
 rm(list = c('cm.header.row', 'data.header.row', 
             'can.header.row'))
+
+comm.can.data %>% 
+  left_join(select(cm.master.list, comm.id, 
+                   comm.name, comm.party, comm.type),
+            by = 'comm.id') %>%
+  left_join(select(can.master.list, can.id, can.name, can.party,
+                   can.el.year, can.office.state, can.office, 
+                   can.office.district, can.incumb.flag,
+                   can.pcc), by = 'can.id')
+
